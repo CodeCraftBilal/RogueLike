2d885b47644344b0dd2cdba75bb7128c2f815f4d
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-03-22 16:39:09 +0500

    added tiles in the ground

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BoardManager : MonoBehaviour
+{
+    private Tilemap m_timeMap;
+    public int width;
+    public int height;
+    public Tile[] GroundTiles;
+    public Tile[] WallTiles;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_timeMap = GetComponentInChildren<Tilemap>();    
+        for(int y = 0; y < height; ++y)
+        {
+            Debug.Log("y: " + y);
+            for(int x = 0; x < width; ++x)
+            {
+                Tile tile;
+                if(x == 0 || y == 0 || x == width - 1 || y == height - 1)
+                {
+                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    
+                } else
+                {
+                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                }
+                m_timeMap.SetTile(new Vector3Int(x, y, 0), tile);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
